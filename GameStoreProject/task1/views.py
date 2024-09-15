85b6644f11a7635293fda6b373cb556eb863625a
verei <verein83@mail.ru>
2024-09-15 21:33:10 +0400

    Выполнено ДЗ 19_3

--- GameStoreProject/task1/views.py
+++ GameStoreProject/task1/views.py
@@ -14,17 +14,14 @@ def cart(request):
     return render(request, 'task1/cart.html')
 
 def create_buyers_and_games(request):
-    # Создание покупателей
     buyer1 = Buyer.objects.create(name='Alice', balance=100.00, age=25)
     buyer2 = Buyer.objects.create(name='Bob', balance=50.00, age=30)
     buyer3 = Buyer.objects.create(name='Charlie', balance=200.00, age=17)
 
-    # Создание игр
     game1 = Game.objects.create(title='Game A', cost=50.00, size=5.0, description='Description A', age_limited=True)
     game2 = Game.objects.create(title='Game B', cost=30.00, size=3.0, description='Description B', age_limited=False)
     game3 = Game.objects.create(title='Game C', cost=40.00, size=4.0, description='Description C', age_limited=True)
 
-    # Связывание покупателей с играми
     game1.buyer.set([buyer1, buyer2])
     game2.buyer.set([buyer1, buyer2, buyer3])
     game3.buyer.set([buyer1, buyer2])
