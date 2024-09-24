3473bc580da9fcb5ccb6cb6e761b8747665797be
mixupp83 <161758839+mixupp83@users.noreply.github.com>
2024-09-24 10:59:29 +0400

    Добавлена страница /create

--- GameStoreProject/task1/urls.py
+++ GameStoreProject/task1/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('shop/', views.shop, name='shop'),
     path('cart/', views.cart, name='cart'),
     path('sign_up/', views.sign_up_by_django, name='sign_up_by_django'),
+    path('create/', views.create_buyers_and_games, name='create_buyers_and_games'),
 ]
